3cdc4b663b673c4fb3e484226d1cdb5dff2bd61d
huangxiaoshen <huangxiaoshen@castelbds.com>
2020-07-29 18:16:05 +0800

    新增开屏view

--- app/src/main/java/com/hxs/viewexercise/MainActivity.kt
+++ app/src/main/java/com/hxs/viewexercise/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.hxs.viewexercise
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testView.animate(2000)
+//        testView.changeColor(Color.GREEN)
     }
 }
--- app/src/main/java/com/hxs/viewexercise/Region1View.kt
+++ app/src/main/java/com/hxs/viewexercise/Region1View.kt
@@ -1,13 +1,14 @@
 package com.hxs.viewexercise
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
 
-class Region1View:View {
+class Region1View : AppCompatImageView {
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
@@ -17,20 +18,67 @@ class Region1View:View {
         defStyleAttr
     )
 
-    private val paint = Paint().apply{
+    private val paint = Paint().apply {
         color = Color.DKGRAY
         flags = Paint.ANTI_ALIAS_FLAG
     }
 
-
-
+    private val progressPath = Path()
+    private var process = 0.2f
+    private var index = 0
+    private val unitHeight = 40f
+    private var color = Color.CYAN
 
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
 
         canvas?.run {
+            println(process)
+            index = 0
+            while (index * unitHeight < measuredHeight) {
+                if (index % 2 == 0) {
+                    progressPath.addRect(
+                        0f,
+                        index * unitHeight,
+                        measuredWidth * process,
+                        (index + 1) * unitHeight,
+                        Path.Direction.CW
+                    )
+                } else {
+                    progressPath.addRect(
+                        (1 - process) * measuredWidth,
+                        index * unitHeight,
+                        measuredWidth.toFloat(),
+                        (index + 1) * unitHeight,
+                        Path.Direction.CW
+                    )
+                }
+                index++
+            }
+//            drawColor(Color.CYAN)
 
+            clipPath(progressPath)
+//            drawColor(color)
+//            drawable.draw(this)
         }
+        super.onDraw(canvas)
     }
+
+    fun animate(duration: Long) {
+        val animator = ValueAnimator.ofFloat(0f, 1f).apply {
+            this.duration = duration
+            addUpdateListener {
+                process = it.animatedValue as Float
+                invalidate()
+            }
+        }
+        animator.start()
+    }
+
+    fun changeColor(color: Int) {
+        this.color = color
+        process = 0.9f
+        invalidate()
+    }
+
 }
